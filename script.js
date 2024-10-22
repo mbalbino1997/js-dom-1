09698b1c5b97b42bd7635af6f6eab6b19cf7e55d
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-10-22 17:18:11 +0200

    add

---
+++ script.js
@@ -0,0 +1,5 @@
+const imgLight=document.querySelector('.light');
+const btnLight=document.querySelector('.btn-light');
+btnLight.addEventListener('click',function(){
+    imgLight.src="./img/yellow_lamp.png"
+})
