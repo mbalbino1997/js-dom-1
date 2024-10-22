a8c4313ae28c85c478da774dd8477f168e7102a3
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-10-22 18:22:46 +0200

    add:bonus

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const imgLight=document.querySelector('.light');
 const btnLight=document.querySelector('.btn-light');
 btnLight.addEventListener('click',function(){
-    imgLight.src="./img/yellow_lamp.png"
+    if(imgLight.src.includes('white_lamp.png')){
+        imgLight.src="./img/yellow_lamp.png"
+        btnLight.textContent= 'Spegni';
+    } else {
+        imgLight.src="./img/white_lamp.png"
+        btnLight.textContent= 'Accendi';
+    }
+    
 })
